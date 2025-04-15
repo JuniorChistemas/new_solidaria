be6ea8c892b5e58fe13523ea2b9f2ad262cd2184
Anthony Marck Mendoza Sanchez <thonymarckyt08@gmail.com>
2025-04-15 15:13:18 -0500

    CREATED OBSERVER FOR MOVEMENTS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Movement;
+use App\Observers\MovementObserver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Movement::observe(MovementObserver::class);
     }
 }
